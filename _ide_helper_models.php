f974ea2b5d9a16e0d02c3dec972f18fc4f37a55a
Mateus Guimarães <mateus.jatene@gmail.com>
2023-07-28 22:35:49 -0300

    05: laravel-ide helper

---
+++ _ide_helper_models.php
@@ -0,0 +1,166 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
+namespace Modules\Order\Models{
+/**
+ * Modules\Order\Models\Order
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $total_in_cents
+ * @property string $status
+ * @property string $payment_gateway
+ * @property string $payment_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order wherePaymentGateway($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order wherePaymentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereTotalInCents($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUserId($value)
+ */
+	class Order extends \Eloquent {}
+}
+
+namespace Modules\Order\Models{
+/**
+ * Modules\Order\Models\OrderLine
+ *
+ * @property int $id
+ * @property int $order_id
+ * @property int $product_id
+ * @property int $product_price_in_cents
+ * @property int $quantity
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine query()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereOrderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereProductPriceInCents($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereQuantity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderLine whereUpdatedAt($value)
+ */
+	class OrderLine extends \Eloquent {}
+}
+
+namespace Modules\Product\Models{
+/**
+ * Modules\Product\Models\CartItem
+ *
+ * @property int $id
+ * @property int $quantity
+ * @property int $user_id
+ * @property int $product_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|CartItem newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|CartItem newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|CartItem query()
+ * @method static \Illuminate\Database\Eloquent\Builder|CartItem whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CartItem whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CartItem whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CartItem whereQuantity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CartItem whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CartItem whereUserId($value)
+ */
+	class CartItem extends \Eloquent {}
+}
+
+namespace Modules\Product\Models{
+/**
+ * Modules\Product\Models\Product
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $price_in_cents
+ * @property int $stock
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Modules\Product\Database\Factories\ProductFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product wherePriceInCents($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereStock($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereUpdatedAt($value)
+ */
+	class Product extends \Eloquent {}
+}
+
+namespace Modules\Shipment\Models{
+/**
+ * Modules\Shipment\Models\Shipment
+ *
+ * @property int $id
+ * @property int $order_id
+ * @property string $provider
+ * @property string $provider_shipment_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereOrderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereProvider($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereProviderShipmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Shipment whereUpdatedAt($value)
+ */
+	class Shipment extends \Eloquent {}
+}
+
