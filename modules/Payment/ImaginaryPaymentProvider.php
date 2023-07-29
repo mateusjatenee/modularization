ca060db9c96443d5791f995342230bd62c2623b0
Mateus Guimarães <mateus.jatene@gmail.com>
2023-07-28 22:36:09 -0300

    Add ImaginaryPaymentProvider

---
+++ modules/Payment/ImaginaryPaymentProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Modules\Payment;
+
+use Illuminate\Support\Str;
+use NumberFormatter;
+
+final class ImaginaryPaymentProvider
+{
+    public function charge(string $token, int $amountInCents, string $statementDescription): array
+    {
+        $this->validateToken($token);
+
+        $numberFormatter = new NumberFormatter('en-US', NumberFormatter::CURRENCY);
+
+        return [
+            'id' => (string) Str::uuid(),
+            'amount_in_cents' => $amountInCents,
+            'localized_amount' => $numberFormatter->format($amountInCents / 100),
+            'statement_description' => $statementDescription,
+            'created_at' => now()->toDateTimeString(),
+        ];
+    }
+
+    public static function make(): ImaginaryPaymentProvider
+    {
+        return new self();
+    }
+
+    /**
+     * @throws \RuntimeException
+     */
+    protected function validateToken(string $token): void
+    {
+        if (! Str::isUuid($token)) {
+            throw new \RuntimeException('The given payment token is not valid.');
+        }
+    }
+}
